1e6764c372e49bad7334236ebb062029d4d09200
Anya <anya@Anyas-MacBook-Pro.local>
2020-12-15 17:35:26 -0500

    I added all my files

---
+++ src/Class1/MyFile.java
@@ -0,0 +1,44 @@
+package Class1;
+
+public class MyFile {
+
+    public static void main(String[] args){
+        /**
+         * Main method test message
+         * code written inside the main method
+         */
+
+        // my test comment
+        /*lalalal
+        llalalalla
+        allalalallal
+        lalallalalla
+        lalallalalalal
+         */
+
+        /**
+         * this is another way to write multy-line comment
+         * Wow!that's great! Love it so much.
+         */
+        System.out.println("My first code message");
+
+        //comment #2
+        /* ho ho ho
+        Merry Christmas
+         */
+
+        System.out.println("Merry Christmas and Happy New Year");
+
+        short newYear = 2021;
+        short daystillnewyear = 15;
+
+        System.out.println(newYear + " the best year in our life");
+
+        System.out.println(daystillnewyear + " days till New " + newYear + " Year");
+
+
+
+
+    }
+
+}
